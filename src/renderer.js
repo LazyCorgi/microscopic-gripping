7d7649e85cd14debcf0ff52a53c891f76d1045ea
Ke <2798595008@qq.com>
2025-04-17 01:12:05 +0800

    格式化/debug

--- src/renderer.js
+++ src/renderer.js
@@ -1,10 +1,10 @@
 // 获取 HTML 元素
-const textInput = document.getElementById("textInput");
-const submitBtn = document.getElementById("submitBtn");
-const output = document.getElementById("output");
+const textInput = document.getElementById('textInput');
+const submitBtn = document.getElementById('submitBtn');
+const output = document.getElementById('output');
 
 // 按钮点击事件
-submitBtn.addEventListener("click", () => {
-    const userInput = textInput.value; // 获取输入框的内容
-    output.innerText = "你输入了: " + userInput; // 修改页面显示的文本
+submitBtn.addEventListener('click', () => {
+    const userInput = textInput.value;            // 获取输入框的内容
+    output.innerText = '你输入了: ' + userInput;  // 修改页面显示的文本
 });
